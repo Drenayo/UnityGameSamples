ca6563f77e78eb2bc7f4d29fb4372c295c145267
梦小天幼 <92071969+Drenayo@users.noreply.github.com>
2023-05-29 22:25:43 +0800

    C# 练习

---
+++ Assets/Z 小Demo/10.C#NET练习/String/CSharp_String.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+namespace Z_10
+{
+
+    public class CSharp_String
+    {
+        [MenuItem("Tools/文件操作")]
+        public static void 文件操作()
+        {
+            Debug.Log("s");
+        }
+    }
+}
+
+
