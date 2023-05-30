6d740f1f23c98ee0521c14b28019525f1cacf4db
梦小天幼 <92071969+Drenayo@users.noreply.github.com>
2023-05-30 08:15:56 +0800

    TimeLine

---
+++ Assets/Z 小Demo/8.TimeLine/Scripts/函数调用测试.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z_8
+{
+    public class 函数调用测试 : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+
+        void Update()
+        {
+            
+        }
+
+        public void 调用测试()
+        {
+            Debug.Log("AAA");
+        }
+    }
+}
