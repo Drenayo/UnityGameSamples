e4b40161b71a761acec0c0268e22f7722d38e1db
梦小天幼 <2090311442@qq.com>
2023-08-16 09:39:34 +0800

    把打字机效果Demo移动到UI文件夹里

---
+++ Assets/Z1_UI相关/1.Text的各种动画/Scripts/静态Text渐变动画.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XXX
+{
+    public class 静态Text渐变动画 : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+
+        void Update()
+        {
+            
+        }
+    }
+}
