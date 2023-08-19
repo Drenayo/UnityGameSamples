393e8ab2fa55d5dbe09b92cb059999d6b6e6290e
梦小天幼 <2090311442@qq.com>
2023-08-19 13:34:30 +0800

    注册登录全功能+1

--- Assets/Z0_Demo/16.UI登录注册功能/3套原登录+功能齐全/LoginSystemAll.cs
+++ Assets/Z0_Demo/16.UI登录注册功能/3套原登录+功能齐全/LoginSystemAll.cs
@@ -8,114 +8,85 @@ namespace Z_16
 {
     public class LoginSystemAll : MonoBehaviour
     {
-        public bool isPlayer;
         public InputField ifd_User;
         public InputField ifd_Pass;
         public InputField ifd_Curr;
-        public GameObject tipsErrorInfo;
-        public Text text_TipsErrorInfo;
-        public GameObject inputStateInfo;
-        public Text text_InputStateInfo;
-        public float tipsHidespeed;
+
+        public GameObject gameObj_InputStateTips;
+        public GameObject gameObj_ErrorTips;
+        public Text txt_InputStateTips;
+        public Text txt_ErrorTips;
+        public float errorTipsHideTime;
         public Toggle tog_ShowPass;
-
-        public Dictionary<string, string> dicUserInfo = new Dictionary<string, string>();
-
+        public bool IsPlayer = true;
+        public Dictionary<string, string> dic_UserInfo = new Dictionary<string, string>();
         private void Start()
         {
-            isPlayer = true;
-            tog_ShowPass.onValueChanged.AddListener(ShowPass);
+
         }
 
         public void Btn_Reg()
         {
-            // 注册 账号密码不能为空、账号已经被注册、账号成功注册
-
-            if (!isPlayer)
+            if (IsPlayer)
             {
-                ShowTipsErrorPanel("游客无需注册，直接登录即可");
-                return;
+                //账号密码不能为空、用户名重复、成功注册
+                if (ifd_User.text == string.Empty || ifd_Pass.text == string.Empty)
+                {
+                    ShowErrorTipsPanel("账号密码不能为空！");
+                }
+                else if (dic_UserInfo.ContainsKey(ifd_User.text))
+                {
+                    ShowErrorTipsPanel("用户名不能重复注册！");
+                }
+                else
+                {
+                    dic_UserInfo.Add(ifd_User.text, ifd_Pass.text);
+                    ShowErrorTipsPanel("注册成功！");
+                    ifd_User.text = string.Empty;
+                    ifd_Pass.text = string.Empty;
+                }
             }
-
-            if (ifd_User.text == string.Empty && ifd_Pass.text == string.Empty)
+            else
             {
-                ShowTipsErrorPanel("账号密码不能为空！");
-                return;
+                ShowErrorTipsPanel("游客无需注册，直接登录即可！");
             }
-            if (dicUserInfo.ContainsKey(ifd_User.text))
-            {
-                ShowTipsErrorPanel("账号已经被注册！");
-                return;
-            }
-            dicUserInfo.Add(ifd_User.text, ifd_Pass.text);
-            ShowTipsErrorPanel("账号注册成功！");
-            ifd_User.text = string.Empty;
-            ifd_Pass.text = string.Empty;
 
         }
 
         public void Btn_Login()
         {
-            // 登录 登录成功、账号密码错误
-            if (!isPlayer)
+            if (IsPlayer)
             {
-                ShowTipsErrorPanel("游客登录成功！");
-                return;
-            }
-
-            if (dicUserInfo.ContainsKey(ifd_User.text) && dicUserInfo.ContainsValue(ifd_Pass.text))
-            {
-                ShowTipsErrorPanel("玩家登录成功！");
-            }
-            else
-            {
-                ShowTipsErrorPanel("账号密码错误，请重新输入！");
-                ifd_Pass.text = string.Empty;
-            }
-        }
-
-        // 得到输入框焦点
-        public void GetInputFocus(string str)
-        {
-            inputStateInfo.SetActive(true);
-            if (str.Equals("账号"))
-            {
-                ifd_Curr = ifd_User;
-                text_InputStateInfo.text = "正在输入账号...";
+                // 不存在该账号 \密码输入错误
+                if (dic_UserInfo.ContainsKey(ifd_User.text))
+                {
+                    string strValue;
+                    if (dic_UserInfo.TryGetValue(ifd_User.text, out strValue))
+                    {
+                        if (strValue.Equals(ifd_Pass.text))
+                            ShowErrorTipsPanel("登录成功！");
+                        else
+                        {
+                            ShowErrorTipsPanel("密码输入错误！");
+                        }
+                    }
+                }
+                else
+                {
+                    ShowErrorTipsPanel("不存在该账号！");
+                }
             }
             else
             {
-                ifd_Curr = ifd_Pass;
-                text_InputStateInfo.text = "正在输入密码...";
+                ShowErrorTipsPanel("登录成功！");
             }
         }
 
-        public void Btn_Number(string str)
+        public void Tog_ShowPass(bool isOn)
         {
-            ifd_Curr.text += str;
-        }
-
-        public void Btn_Claer()
-        {
-            ifd_Curr.text = string.Empty;
-        }
-
-        public void Btn_Remove()
-        {
-            if (ifd_Curr.text.Length != 0)
-                ifd_Curr.text = ifd_Curr.text.Substring(0, ifd_Curr.text.Length - 1);
-        }
-
-        public void SetIsPlayer(bool b)
-        {
-            isPlayer = b;
-        }
-
-        public void ShowPass(bool isShow)
-        {
-            if (isShow)
+            if (isOn)
             {
-                ifd_Pass.contentType = InputField.ContentType.Alphanumeric;
+                ifd_Pass.contentType = InputField.ContentType.Standard;
                 ifd_Pass.ActivateInputField();
             }
             else
@@ -125,18 +96,54 @@ namespace Z_16
             }
         }
 
-        public void ShowTipsErrorPanel(string str)
+        public void SetUserPlayer(bool b)
         {
-            tipsErrorInfo.SetActive(true);
-            text_TipsErrorInfo.text = str;
-            StopAllCoroutines();
-            StartCoroutine(ShowTipsErrorPanel_E());
+            IsPlayer = b;
         }
 
-        IEnumerator ShowTipsErrorPanel_E()
+        // 得到焦点
+        public void GetInputFoucePoint(string str)
         {
-            yield return new WaitForSeconds(tipsHidespeed);
-            tipsErrorInfo.SetActive(false);
+            gameObj_InputStateTips.SetActive(true);
+            if (str.Equals("账号"))
+            {
+                txt_InputStateTips.text = "正在输入账号..";
+                ifd_Curr = ifd_User;
+            }
+            else
+            {
+                txt_InputStateTips.text = "正在输入密码..";
+                ifd_Curr = ifd_Pass;
+            }
+        }
+
+        public void Btn_Number(string number)
+        {
+            ifd_Curr.text += number;
+        }
+
+        public void Btn_Remove()
+        {
+            if(ifd_Curr.text.Length > 0)
+            ifd_Curr.text = ifd_Curr.text.Substring(0, ifd_Curr.text.Length - 1);
+        }
+
+        public void Btn_Clear()
+        {
+            ifd_Curr.text = string.Empty;
+        }
+
+        private void ShowErrorTipsPanel(string strContent)
+        {
+            gameObj_ErrorTips.SetActive(true);
+            gameObj_ErrorTips.GetComponent<Text>().text = strContent;
+            StartCoroutine(ShowErrorTipsPanel_E());
+        }
+
+        IEnumerator ShowErrorTipsPanel_E()
+        {
+            yield return new WaitForSeconds(errorTipsHideTime);
+            gameObj_ErrorTips.SetActive(false);
         }
     }
 }
