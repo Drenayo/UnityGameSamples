4106e5f5e3f21aacb168e09cceb0302d5332c237
梦小天幼 <drenayo@qq.com>
2024-04-27 10:12:44 +0800

    上传Editor工具、删除空文件夹

---
+++ Assets/Editor/CreateScriptableObjectEditor.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
+using System;
+using System.Linq;
+using Sirenix.OdinInspector.Editor;
+using System.Reflection;
+
+public class CreateScriptableObjectEditor : OdinEditorWindow
+{
+    public string soName = "NewScriptableObject";
+    [FolderPath]
+    public string soLocation = "Assets/";
+    public string typeStr;
+    private Type sotype;
+
+    [MenuItem("Tools/创建SO工具")]
+    private static void OpenWindow()
+    {
+        var w = GetWindow<CreateScriptableObjectEditor>();
+        w.Show();
+        w.titleContent = new GUIContent("创建SO工具");
+
+    }
+
+    [Button("创建",ButtonSizes.Medium)]
+    public void Btn()
+    {
+        CreateScriptableObject(FindTypeByName(typeStr));
+    }
+
+    private void CreateScriptableObject(Type type)
+    {
+        ScriptableObject so = ScriptableObject.CreateInstance(type);
+        string assetPath = AssetDatabase.GenerateUniqueAssetPath(soLocation + soName + ".asset");
+        AssetDatabase.CreateAsset(so, assetPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    // 根据类型名查找类型
+    public Type FindTypeByName(string typeName)
+    {
+        // 获取当前应用程序域中所有已加载的程序集
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        // 在每个程序集中查找具有指定名称的类型
+        foreach (var assembly in assemblies)
+        {
+            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == typeName);
+            if (type != null)
+            {
+                return type;
+            }
+        }
+        return null;
+    }
+}
