306b8d042172ebf1bd0c337bc343d9aebedc4142
梦小天幼 <drenayo@qq.com>
2024-07-23 15:56:03 +0800

    Update CodeLineCounterTool_Odin.cs

--- Assets/Editor/E001-代码行数统计工具/Scripts/CodeLineCounterTool_Odin.cs
+++ Assets/Editor/E001-代码行数统计工具/Scripts/CodeLineCounterTool_Odin.cs
@@ -3,6 +3,7 @@ using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace E001
 {
@@ -10,14 +11,15 @@ namespace E001
     {
         [FolderPath(AbsolutePath = true)]
         [OnValueChanged("OnValueChanged")]
+        [LabelText("代码文件夹路径")]
         public string folderPath;
+        private int totalLines;
 
-        public int totalLines;
-
-        [MenuItem("Tools/Editor/E001-代码行数统计工具",priority = 1)]
+        [MenuItem("Tools/Editor/E001-代码行数统计工具", priority = 1)]
         public static void ShowWindow()
         {
             GetWindow<CodeLineCounter>().Show();
+            GetWindow<CodeLineCounter>().titleContent = new GUIContent("代码行数统计工具");
         }
 
         public void OnValueChanged()
@@ -38,7 +40,22 @@ namespace E001
             }
             return totalLines;
         }
+
+        protected override void OnGUI()
+        {
+            base.OnGUI();
+            EditorGUILayout.Space();
+            GUIStyle style = new GUIStyle(EditorStyles.label)
+            {
+                fontSize = 25,
+                alignment = TextAnchor.MiddleCenter
+            };
+            EditorGUILayout.LabelField(totalLines.ToString(), style);
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+        }
     }
 }
-
 #endif
